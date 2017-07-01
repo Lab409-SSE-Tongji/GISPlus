9e8dd65d164a0dde378377907a3f290422782294
justin <iheyu22@gmail.com>
2017-07-01 12:11:14 +0800

    初始化后端SpringBoot框架

---
+++ backend/src/main/java/web/rainlf/WebMapEditorApplication.java
@@ -0,0 +1,12 @@
+package web.rainlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMapEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMapEditorApplication.class, args);
+	}
+}
