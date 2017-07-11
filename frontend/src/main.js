1845199fc267c259fda6ba4f6330a333a7f3de49
徐啊歪 <1163515336@qq.com>
2017-07-11 11:06:17 +0800

    user profile with static data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,12 +6,14 @@ import store from './store'
 import router from './router'
 import VueResource from 'vue-resource'
 import VeeValidate from 'vee-validate'
-
+import ElementUI from 'element-ui'
+import '../static/css/theme-green/index.css';       // 浅绿色主题
 
 import './global'
 
 Vue.use(VueResource);
 Vue.use(VeeValidate);
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 // Vue.http.interceptors.push(function(request, next) {
