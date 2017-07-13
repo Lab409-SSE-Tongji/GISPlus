28c2ed319f13136fa078583d50a82a67f4a0935b
justin <iheyu22@gmail.com>
2017-07-13 15:33:41 +0800

    注释

--- backend/src/main/java/com/rainlf/controller/RecycleController.java
+++ backend/src/main/java/com/rainlf/controller/RecycleController.java
@@ -18,21 +18,41 @@ public class RecycleController {
     @Autowired
     private RecycleService recycleService;
 
+    /**
+     * 获取用户所有回收站地图
+     * @param userId
+     * @return
+     */
     @RequestMapping(value = "{userId}", method = RequestMethod.GET)
     public ResponseEntity getRecycles(@PathVariable("userId") String userId) {
         return ResponseEntity.ok(recycleService.getRecycles(userId));
     }
 
+    /**
+     * 还原用户地图
+     * @param recycleId
+     * @return
+     */
     @RequestMapping(value = "restore/{recycleId}", method = RequestMethod.POST)
     public ResponseEntity restoreRecycle(@PathVariable("recycleId") String recycleId) {
         return ResponseEntity.ok(recycleService.restoreRecycle(recycleId));
     }
 
+    /**
+     * 删除回收站地图
+     * @param recycleId
+     * @return
+     */
     @RequestMapping(value = "{recycleId}", method = RequestMethod.DELETE)
     public ResponseEntity deleteRecycle(@PathVariable("recycleId") String recycleId) {
         return ResponseEntity.ok(recycleService.deleteRecycle(recycleId));
     }
 
+    /**
+     * 清空回收站地图
+     * @param userId
+     * @return
+     */
     @RequestMapping(value = "all/{userId}", method = RequestMethod.DELETE)
     public ResponseEntity deleteRecycles(@PathVariable("userId") String userId) {
         return ResponseEntity.ok(recycleService.deleteRecycles(userId));
--- backend/src/main/java/com/rainlf/controller/WaterPipeLayerController.java
+++ backend/src/main/java/com/rainlf/controller/WaterPipeLayerController.java
@@ -18,26 +18,52 @@ public class WaterPipeLayerController {
     @Autowired
     private WaterPipeService waterPipeService;
 
+    /**
+     * 获取下水管道图层
+     * @param mapId
+     * @return
+     */
     @RequestMapping(value = "{mapId}", method = RequestMethod.GET)
     public ResponseEntity getWaterPipeLayer(@PathVariable("mapId") String mapId) {
         return ResponseEntity.ok(waterPipeService.getWaterPipeLayer(mapId));
     }
 
+    /**
+     * 添加空的下水管道图层
+     * @param mapId
+     * @return
+     */
     @RequestMapping(value = "{mapId}", method = RequestMethod.POST)
     public ResponseEntity addWaterPipeLayer(@PathVariable("mapId") String mapId) {
         return ResponseEntity.ok(waterPipeService.addWaterPipeLayer(mapId));
     }
 
+    /**
+     * 删除下水管道图层
+     * @param layerId
+     * @return
+     */
     @RequestMapping(value = "{layerId}", method = RequestMethod.DELETE)
     public ResponseEntity deleteWaterPipeLayer(@PathVariable("layerId") String layerId) {
         return ResponseEntity.ok(waterPipeService.deleteWaterPipeLayer(layerId));
     }
 
+    /**
+     * 导入下水管道图层
+     * @param layerId
+     * @param file
+     * @return
+     */
     @RequestMapping(value = "excel/{layerId}", method = RequestMethod.POST)
     public ResponseEntity importWaterPipeLayer(@PathVariable("layerId") String layerId, @RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok(waterPipeService.importWaterPipeLayer(layerId, file));
     }
 
+    /**
+     * 导出下水管道图层
+     * @param layerId
+     * @return
+     */
     @RequestMapping(value = "excel/{layerId}", method = RequestMethod.GET)
     public ResponseEntity exportWaterPipeLayer(@PathVariable("layerId") String layerId) {
         return ResponseEntity.ok(waterPipeService.exportWaterPipeLayer(layerId));
